5aeea9aabebb344e2ac1645cf298d9112c755aeb
heihaheihahello <97714936+heihaheihahello@users.noreply.github.com>
2022-02-02 16:56:40 -0800

    fix divide bug1
    fix bugs of int/int = int
    now it will return the string of float form correctly

--- divide.java
+++ divide.java
@@ -3,7 +3,9 @@ public class divide {
     public String div(String toSolve) {
         String[] nums = toSolve.split("/");
         int num1 = Integer.parseInt(nums[0]);
+        double num1_new = num1;
         int num2 = Integer.parseInt(nums[1]);
-        return String.valueOf(num1/num2);
+        double num2_new = num2;
+        return String.valueOf(num1_new/num2_new);
     }
 }
